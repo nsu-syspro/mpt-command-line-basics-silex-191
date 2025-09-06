438b8c0e70a4be7c646f2507f818a56b980ba9d2
Stepan Kargin <Stepan.RomKa191@gmail.com>
2025-09-06 12:10:20 +0700

    Update solution

--- solution/task1.sh
+++ solution/task1.sh
@@ -3,6 +3,6 @@ TMP_FILE=$(mktemp -d)
 tar -xf archive-part1.tar -C $TMP_FILE
 pwd
 unzip archive-part2.zip -d $TMP_FILE
-tar -czf archive-part3.tar.gz -C $TMP_FILE .
+tar -czf archive-combined.tar.gz -C $TMP_FILE .
 rm -r $TMP_FILE
 
