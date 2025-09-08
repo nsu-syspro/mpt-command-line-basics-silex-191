ad15f10093176e3d98d2e77ab9a30234fc9065a6
Stepan Kargin <Stepan.RomKa191@gmail.com>
2025-09-08 21:49:55 +0700

    After review

--- solution/task3.sh
+++ solution/task3.sh
@@ -1,4 +1,1 @@
-TMP_FILE=$(mktemp)
-find . -name '*.txt' -and -type f -exec basename '{}' \; > $TMP_FILE
-sort $TMP_FILE
-rm $TMP_FILE
+find . -name '*.txt' -and -type f -exec basename '{}' \; | sort
